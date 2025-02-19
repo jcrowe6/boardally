f53606779bb8d5f6f9c67c04861f01b725ce20c8
Jeremiah Crowell <jeremiah.c2001@gmail.com>
2025-02-19 16:01:24 -0600

    full doc from DynamoDB

--- pages/api/query.ts
+++ pages/api/query.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
-import { DynamoDBDocumentClient, GetCommand, PutCommand, UpdateCommand } from "@aws-sdk/lib-dynamodb"
+import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb"
 import { RAGResponse } from "../../schemas/rag/ragResponseSchema";
 import { RAGRequestSchema } from "../../schemas/rag/ragRequestSchema";
 import { z } from "zod";
@@ -18,7 +18,46 @@ const getRequiredEnvVar = (name: string): string => {
   return value;
 };
 
-const get_prompt = (question: string, hits: Hit[]): string => {
+const client = new DynamoDBClient({
+  region: "us-east-2"
+});
+
+const docClient = DynamoDBDocumentClient.from(client);
+
+export const getRulebookTextFromGameId = async(gameId: string) => {
+  const command = new QueryCommand({
+      TableName: "boardally-rulebook-metadata",
+      KeyConditionExpression: "#id = :id",
+      ExpressionAttributeValues: {
+        ":id": gameId
+      },
+      ExpressionAttributeNames: {
+        "#id": "game_id"
+      }
+  })
+  
+  try {
+    const response = await docClient.send(command);
+    const items = response.Items
+    if (items === undefined) {
+      throw Error("Failed on call to DB")
+    } else if (items.length == 0) { 
+      throw Error("Got 0 rulebooks")
+    } else if (items.length > 1) { 
+      throw Error("Got multiple rulebooks")
+    } else {
+      const rulebook = items[0]["ocr_md"]
+      if (!rulebook) {
+        throw Error("No scan for rulebook")
+      }
+      return rulebook
+    }
+  } catch (error) {
+      throw error
+  }
+}
+
+const getPromptWithHits = (question: string, hits: Hit[]): string => {
   const ref_texts = hits.map((hit) => `[Page ${hit.fields.page_num}]\n${hit.fields.text}`)
   return `Rulebook pages:\n${ref_texts.join("\n")}\n\n
           User question: "${question}"
@@ -27,6 +66,15 @@ const get_prompt = (question: string, hits: Hit[]): string => {
           referencing the rulebook pages. \
           If the answer cannot be determined from the rules, say "I'm sorry, I can't determine the answer to your question". \
           Be sure to cite the page(s) you used.`;
+}
+
+const getPromptWithFullRulebook = (question: string, rulebook: string): string => {
+  return `Rulebook:\n${rulebook}\n
+          User question: "${question}"
+          Instruction:
+          Provide a concise answer to the user's rules question, \
+          referencing the rulebook. \
+          Be sure to cite the sections you used.`;
 }
 
 export default async function handler(
@@ -49,43 +97,10 @@ export default async function handler(
     const valid_request = RAGRequestSchema.parse(req.body);
 
     // Call Retrieval service
-    const retr_req: PineconeRequest = {
-      query: {
-        inputs: {
-          text: valid_request["question"]
-        },
-        filter: {
-          chunk_type: "page"
-        },
-        top_k: 3
-      },
-      fields: ["text", "page_num"]
-    }
-    
-    const pc_uri = `${env.PINECONE_HOST}/records/namespaces/${valid_request["game"]}/search`
-    const retrieval_response = await fetch(pc_uri, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Api-Key': env.PINECONE_API_KEY, 
-        'X-Pinecone-API-Version': 'unstable'
-      },
-      body: JSON.stringify(retr_req),
-    })
-    
-    if (!retrieval_response.ok) {
-      console.error("Error from retrieval service:", retrieval_response.statusText);
-      res.status(502).json({ answer: "Error querying retrieval service" });
-      return;
-    }
-
-    const retrieval_data = await retrieval_response.json()
-
-    const valid_retr_response = PineconeResponseSchema.parse(retrieval_data);
+    const rulebookText = await getRulebookTextFromGameId(valid_request.game)
     
     // Construct prompt and call generation service
-    const prompt = get_prompt(valid_request.question, valid_retr_response.result.hits)
+    const prompt = getPromptWithFullRulebook(valid_request.question, rulebookText)
     console.log(prompt)
     const gen_req: FireworksRequest = {
       model: env.FIREWORKS_MODEL,
